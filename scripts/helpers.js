56ed9eb2d850ce54729b9d71cc1427415e49d599
betachen <betachen@yeah.net>
2017-11-23 18:40:23 +0800

    overwrite package name

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -98,7 +98,7 @@ hexo.extend.helper.register('url_for_lang', function(path) {
 });
 
 hexo.extend.helper.register('raw_link', function(path) {
-  return 'https://github.com/mvs-org/mvs-docs/edit/master/source/' + path;
+  return 'https://github.com/mvs-org/docs-mvs-org/edit/master/source/' + path;
 });
 
 hexo.extend.helper.register('page_anchor', function(str) {
